705a66eff8865a4d2a8630aa5ccb7154624ae2ae
kram161 <39655243+kram161@users.noreply.github.com>
2021-11-18 04:16:58 +0300

    upd

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,40 @@
 console.log('scripts/main.js');
 
 window.addEventListener('load', function() {
-    const ta = document.querySelector('#ta')
-    const taNumber = document.querySelector('#taNumber')
+    const ta = document.querySelector('#ta, #taAddPhotoPhoto')
+    const taNumber = document.querySelector('#taNumber,#taNumberAddPhotoPhoto')
     ta.addEventListener("input", function(){
         taNumber.innerText = ta.value.length;
+    });
+    const taAddPhotoEpisode = document.querySelector('#taAddPhotoEpisode')
+    const taNumberAddPhotoEpisode = document.querySelector('#taNumberAddPhotoEpisode')
+    taAddPhotoEpisode.addEventListener("input", function(){
+        taNumberAddPhotoEpisode.innerText = taAddPhotoEpisode.value.length;
     });
 });
 
 
-let input = document.querySelector("#interestsTa");
+let interestsTaPhoto = document.querySelector("#interestsTa,#interestsTaPhoto,#interestsTaPhotoEpisode");
+interestsTaPhoto.addEventListener("keypress", function (evt) {
+
+    let words = this.value.split(/\s+/);
+    let numWords = words.length;
+    let maxWords = 5;
+    if (numWords > maxWords) {
+        evt.preventDefault();
+    }
+});
+let interestsTaPhotoEpisode = document.querySelector("#interestsTaPhoto");
+interestsTaPhotoEpisode.addEventListener("keypress", function (evt) {
+
+    let words = this.value.split(/\s+/);
+    let numWords = words.length;
+    let maxWords = 5;
+    if (numWords > maxWords) {
+        evt.preventDefault();
+    }
+});
+let input = document.querySelector("#interestsTaPhotoEpisode");
 input.addEventListener("keypress", function (evt) {
 
     let words = this.value.split(/\s+/);
@@ -19,3 +44,4 @@ input.addEventListener("keypress", function (evt) {
         evt.preventDefault();
     }
 });
+
