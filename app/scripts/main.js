df9d3546c000f3438e0d94a834ef76ae608686d4
kram161 <39655243+kram161@users.noreply.github.com>
2021-11-19 03:38:27 +0300

    upd

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,49 +1,63 @@
 console.log('scripts/main.js');
 
-window.addEventListener('load', function() {
-    const ta = document.querySelector('#ta, #taAddPhotoPhoto')
-    const taNumber = document.querySelector('#taNumber,#taNumberAddPhotoPhoto')
-    ta.addEventListener("input", function(){
-        taNumber.innerText = ta.value.length;
+try{
+    window.addEventListener('load', function() {
+        const ta = document.querySelector('#ta, #taAddPhotoPhoto')
+        const taNumber = document.querySelector('#taNumber,#taNumberAddPhotoPhoto')
+        ta.addEventListener("input", function(){
+            taNumber.innerText = ta.value.length;
+        });
+        const taAddPhotoEpisode = document.querySelector('#taAddPhotoEpisode')
+        const taNumberAddPhotoEpisode = document.querySelector('#taNumberAddPhotoEpisode')
+        taAddPhotoEpisode.addEventListener("input", function(){
+            taNumberAddPhotoEpisode.innerText = taAddPhotoEpisode.value.length;
+        });
     });
-    const taAddPhotoEpisode = document.querySelector('#taAddPhotoEpisode')
-    const taNumberAddPhotoEpisode = document.querySelector('#taNumberAddPhotoEpisode')
-    taAddPhotoEpisode.addEventListener("input", function(){
-        taNumberAddPhotoEpisode.innerText = taAddPhotoEpisode.value.length;
+
+    window.addEventListener('load', function() {
+        const taAddPhotoPhoto = document.querySelector('#taAddPhotoPhoto')
+        const taNumberAddPhotoPhoto = document.querySelector('#taNumberAddPhotoPhoto')
+        taAddPhotoPhoto.addEventListener("input", function(){
+            taNumberAddPhotoPhoto.innerText = taAddPhotoPhoto.value.length;
+        });
     });
-});
 
 
-let interestsTaPhoto = document.querySelector("#interestsTa,#interestsTaPhoto,#interestsTaPhotoEpisode");
-interestsTaPhoto.addEventListener("keypress", function (evt) {
 
-    let words = this.value.split(/\s+/);
-    let numWords = words.length;
-    let maxWords = 5;
-    if (numWords > maxWords) {
-        evt.preventDefault();
-    }
-});
-let interestsTaPhotoEpisode = document.querySelector("#interestsTaPhoto");
-interestsTaPhotoEpisode.addEventListener("keypress", function (evt) {
+    let interestsTaPhoto = document.querySelector("#interestsTa,#interestsTaPhoto,#interestsTaPhotoEpisode");
+    interestsTaPhoto.addEventListener("keypress", function (evt) {
 
-    let words = this.value.split(/\s+/);
-    let numWords = words.length;
-    let maxWords = 5;
-    if (numWords > maxWords) {
-        evt.preventDefault();
-    }
-});
-let input = document.querySelector("#interestsTaPhotoEpisode");
-input.addEventListener("keypress", function (evt) {
+        let words = this.value.split(/\s+/);
+        let numWords = words.length;
+        let maxWords = 5;
+        if (numWords > maxWords) {
+            evt.preventDefault();
+        }
+    });
+    let interestsTaPhotoEpisode = document.querySelector("#interestsTaPhoto");
+    interestsTaPhotoEpisode.addEventListener("keypress", function (evt) {
 
-    let words = this.value.split(/\s+/);
-    let numWords = words.length;
-    let maxWords = 5;
-    if (numWords > maxWords) {
-        evt.preventDefault();
-    }
-});
+        let words = this.value.split(/\s+/);
+        let numWords = words.length;
+        let maxWords = 5;
+        if (numWords > maxWords) {
+            evt.preventDefault();
+        }
+    });
+    let input = document.querySelector("#interestsTaPhotoEpisode");
+    input.addEventListener("keypress", function (evt) {
+
+        let words = this.value.split(/\s+/);
+        let numWords = words.length;
+        let maxWords = 5;
+        if (numWords > maxWords) {
+            evt.preventDefault();
+        }
+    });
+}
+catch {
+
+}
 document.getElementById('inputActivator').onclick = function() {
     document.getElementById('helperActive').classList.toggle('menu__first-block__search-box__short-active');
     document.getElementById('inputActive').classList.toggle('menu__first-block__search-box-active');
@@ -60,3 +74,10 @@ document.getElementById('inputActivator').onclick = function() {
     document.addEventListener('touchstart', menuCloseClickOutside);
 }
 
+
+document.getElementById("burgerActivator").onclick = function() {open()};
+
+function open() {
+    document.querySelector(".menu__first-block__burger__button").classList.toggle("burger-active");
+    document.querySelector(".menu__first-block__burger__block").classList.toggle("menu-active");
+}
